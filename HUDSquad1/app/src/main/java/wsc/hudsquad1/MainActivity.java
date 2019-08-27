5928887e1fcde1d4e71a4de84d106cc81dd2e06c
Rishabh <ojas14@gmail.com>
2019-08-28 03:37:05 +1000

    First Prototype

--- HUDSquad1/app/src/main/java/wsc/hudsquad1/MainActivity.java
+++ HUDSquad1/app/src/main/java/wsc/hudsquad1/MainActivity.java
@@ -2,12 +2,96 @@ package wsc.hudsquad1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar speedSet;
+    TextView speed, time, distance;
+    BigDecimal dist = BigDecimal.valueOf(-0.1);
+    ImageView left, right;
+    int delay = 5000;
+    int period = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        speedSet = findViewById(R.id.seekBar);
+        speed = findViewById(R.id.textView);
+        time = findViewById((R.id.textView2));
+        distance = findViewById(R.id.textView4);
+        left = findViewById(R.id.imageView);
+        right = findViewById(R.id.imageView2);
+
+        speedSet.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                speed.setText(String.valueOf(progress));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        Date curTime = Calendar.getInstance().getTime();
+        time.setText(curTime.toString());
+
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                dist = dist.add(BigDecimal.valueOf(0.1));
+                distance.setText((String.valueOf(dist)));
+            }
+        } ,delay, period);
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    public void run() {
+                        if (left.getDrawable() == null)
+                            left.setImageResource(R.drawable.turnleft);
+
+                        else
+                            left.setImageDrawable(null);
+
+                        if (right.getDrawable() == null)
+                            right.setImageResource(R.drawable.turnright);
+
+                        else
+                            right.setImageDrawable(null);
+                    }
+                });
+
+            }
+        } ,0, 500);
+
+
+
     }
 }
