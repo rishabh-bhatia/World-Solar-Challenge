8ea759d1ecf89bc2b14d357d01d7d734d5f8ef09
Rishabh <ojas14@gmail.com>
2019-09-04 10:36:31 +1000

    Application under testing

--- HUDSquad1/app/src/main/java/wsc/hudsquad1/MainActivity.java
+++ HUDSquad1/app/src/main/java/wsc/hudsquad1/MainActivity.java
@@ -1,5 +1,6 @@
 package wsc.hudsquad1;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -17,14 +18,20 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    SeekBar speedSet;
-    TextView speed, time, distance;
+    //SeekBar speedSet;//Seekbar object
+    TextView speed, time, distance;//Textview objects
     //BigDecimal dist = new BigDecimal(0.1);
-    double dist = 0;
-    ImageView left, right;
-    int delay = 0;
-    int period = 1000;
-    double s;
+    double dist = 0;//Distance travelled
+    ImageView left, right, hazard;//Imageview objects
+    int delay = 0;//Time taken by the timer before the first execution
+    int period = 500;//Interval after which the timer repeats
+    int s = 0;//Value of ProgressBar's realtime position based on which odometric calculations are conducted
+    double ss;//double value of s for distance calculation
+    int speedDelay = 0;
+    int speedPeriod = 500;
+    int flag = 0;//Flag for speed timer
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +40,16 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        speedSet = findViewById(R.id.seekBar);
+        //speedSet = findViewById(R.id.seekBar);
         speed = findViewById(R.id.textView);
         time = findViewById((R.id.textView2));
         distance = findViewById(R.id.textView4);
         left = findViewById(R.id.imageView);
         right = findViewById(R.id.imageView2);
+        hazard = findViewById(R.id.imageView10);
 
-        speedSet.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+        /*speedSet.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
@@ -58,28 +67,57 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
-        });
+        });*/
 
         Date curTime = Calendar.getInstance().getTime();
         time.setText(curTime.toString());
 
-        Timer timer = new Timer();
+        Timer timer = new Timer();//Timer initialization
 
+        //Speed timer
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if(flag == 0)
+                {
+                    s = s + 5;
+                    if (s == 180)
+                        flag = 1;
+                    speed.setText(String.valueOf(s));
+                }
 
+                else if (flag == 1)
+                {
+
+                    s = s - 5;
+                    if (s == 0)
+                        flag = 0;
+                    speed.setText(String.valueOf(s));
+
+                }
+            }
+        } , speedDelay, speedPeriod);
+
+        //Odometer timer
        timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 /*dist = dist.add(BigDecimal.valueOf(0.1));
                 distance.setText((String.valueOf(dist)));*/
-                dist = dist + s/3600;
-                distance.setText(String.format("%.1f", dist));
+                /*Odometric calculation by using Distance = Speed * time
+                where time  = 1hr = [1/(60*60)]secs*/
+                ss = s;
+                dist = dist + ss/3600;
+                distance.setText(String.format("%.1f", dist) + "km");//Show distance upto 1 decimal place in km
 
 
             }
-        } ,delay, period);
+        } , delay, period);
 
 
 
+
+       //Indicator timer: Run timer every 500 milliseconds and flash indicators + Hazard Icon
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
@@ -97,11 +135,16 @@ public class MainActivity extends AppCompatActivity {
                         else
                             right.setImageDrawable(null);
 
+                        if (hazard.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.hazardson).getConstantState())
+                            hazard.setImageResource(R.drawable.hazardsoff);
+
+                        else
+                            hazard.setImageResource(R.drawable.hazardson);
                     }
                 });
 
             }
-        } ,0, 500);
+        } , delay, period);
 
 
 
